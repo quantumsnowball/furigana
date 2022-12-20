a14a1c113cbc0dcf2fd09b64939b16b764a66b1d
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-20 19:19:38 +0800

    layout: source and result textfields

--- components/Page/Home/index.tsx
+++ components/Page/Home/index.tsx
@@ -1,4 +1,5 @@
 import { styled, TextField } from "@mui/material";
+import { useState } from "react";
 import { Overflow, Stretch } from "../../styled/containers";
 
 
@@ -6,14 +7,32 @@ const ContentDiv = styled(Overflow(Stretch('div')))`
 `
 
 function Home() {
+  const [text, setText] = useState('Furigana')
+
   return (
     <ContentDiv id='content-ctn'>
       <TextField
         fullWidth
         multiline
+        label='Japanese'
         sx={{
+          my: 2,
           p: 1,
         }}
+        value={text}
+        onChange={e => setText(e.target.value)}
+      >
+
+      </TextField>
+      <TextField
+        fullWidth
+        multiline
+        label='Furigana, Romaji, Translation'
+        sx={{
+          my: 2,
+          p: 1,
+        }}
+        value={text}
       >
 
       </TextField>
