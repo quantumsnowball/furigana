c275c88ff6f363ae1abcba0f0706c106015f4744
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-20 22:51:42 +0800

    loaded and converted from hiragana to furigana ruby text

--- components/Page/Home/index.tsx
+++ components/Page/Home/index.tsx
@@ -1,13 +1,24 @@
-import { styled, TextField } from "@mui/material";
-import { useState } from "react";
+import { styled, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { Overflow, Stretch } from "../../styled/containers";
+import Kuroshiro from "kuroshiro"
+import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji"
 
 
 const ContentDiv = styled(Overflow(Stretch('div')))`
 `
 
 function Home() {
-  const [text, setText] = useState('Furigana')
+  const [source, setSource] = useState('Furigana')
+  const [furigana, setFurigana] = useState('Furigana')
+  const kuroshiro = new Kuroshiro()
+
+  useEffect(() => {
+    const init = async () => {
+      await kuroshiro.init(new KuromojiAnalyzer({ dictPath: "dict/" }))
+    }
+    init()
+  }, [])
 
   return (
     <ContentDiv id='content-ctn'>
@@ -19,8 +30,18 @@ function Home() {
           my: 2,
           p: 1,
         }}
-        value={text}
-        onChange={e => setText(e.target.value)}
+        value={source}
+        onChange={async e => {
+          setSource(e.target.value)
+          try {
+            const converted = await kuroshiro.convert(e.target.value, {
+              mode: "furigana", to: "hiragana"
+            })
+            setFurigana(converted)
+          } catch (err) {
+            console.log(err)
+          }
+        }}
       >
 
       </TextField>
@@ -32,10 +53,13 @@ function Home() {
           my: 2,
           p: 1,
         }}
-        value={text}
+        value={furigana}
       >
 
       </TextField>
+      <ruby>
+        {furigana}
+      </ruby>
     </ContentDiv>
   )
 }
