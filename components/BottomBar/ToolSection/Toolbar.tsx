90afab07ce61d90283b6b938a182879e6331e19b
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-22 07:48:18 +0800

    layout: toolbox only appear in workspace

---
+++ components/BottomBar/ToolSection/Toolbar.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/system"
+
+function Toolbar() {
+  return (
+    <Box
+      sx={{ flex: 1 }}
+    >
+      toolbox
+    </Box>
+  )
+}
+export default Toolbar
--- components/BottomBar/ToolSection/index.tsx
+++ components/BottomBar/ToolSection/index.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/system"
+import { useRouter } from "next/router"
+import Toolbar from "./Toolbar"
 
 function ToolSection() {
-  return (
-    <Box
-      sx={{ flex: 1 }}
-    >
-    </Box>
-  )
+  const router = useRouter()
+
+  return router.pathname === '/' ?
+    <Toolbar />
+    :
+    <Box sx={{ flex: 1 }} > </Box>
 }
 
 export default ToolSection
