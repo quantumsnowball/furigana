3083d3f6b0e273b45b596e06aaba313bada7f59d
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-21 13:27:38 +0800

    feat: source text editor moved to a fullscreen dialog

---
+++ components/Page/Home/Editor.tsx
@@ -0,0 +1,84 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material"
+import { Dispatch, SetStateAction, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { contentActions } from "../../../redux/slices/contentSlice"
+import { RootState } from "../../../redux/store"
+import Kuroshiro from "kuroshiro"
+import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji"
+
+
+const kuroshiro = new Kuroshiro();
+(async () => await kuroshiro.init(new KuromojiAnalyzer({ dictPath: "dict/" })))()
+
+interface EditorProps {
+  editorOpen: boolean,
+  setEditorOpen: Dispatch<SetStateAction<boolean>>
+}
+
+function Editor({ editorOpen, setEditorOpen }: EditorProps) {
+  const dispatch = useDispatch()
+  const [source, setSource] = [
+    useSelector((s: RootState) => s.content.source),
+    (s: string[]) => dispatch(contentActions.setSource(s))
+  ]
+  const setFurigana =
+    (s: string[]) => dispatch(contentActions.setFurigana(s))
+
+  const setRomaji =
+    (s: string[]) => dispatch(contentActions.setRomaji(s))
+
+  const [sourceText, setSourceText] = useState(source.join('\n'))
+
+  const convert = (mode: string, to: string) =>
+    async (txt: string) => await kuroshiro.convert(txt, { mode, to })
+
+  const onClose = () => setEditorOpen(false)
+  const onConfirm = async () => {
+    const vals = sourceText.split('\n')
+    setSource(vals)
+    try {
+      setFurigana(await Promise.all(vals.map(
+        async (txt: string) => await convert('furigana', 'hiragana')(txt))))
+      setRomaji(await Promise.all(vals.map(
+        async (txt: string) => await convert('furigana', 'romaji')(txt))))
+      setEditorOpen(false)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <Dialog
+      fullScreen
+      open={editorOpen}
+      onClose={onClose}
+      aria-labelledby="responsive-dialog-title"
+    >
+      <DialogTitle id="responsive-dialog-title">
+        Source Text
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Enter Japanese text.
+        </DialogContentText>
+        <TextField
+          fullWidth
+          multiline
+          variant='filled'
+          value={sourceText}
+          onChange={e => setSourceText(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button autoFocus onClick={onClose}>
+          Cancel
+        </Button>
+        <Button onClick={onConfirm} autoFocus>
+          OK
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default Editor
--- components/Page/Home/index.tsx
+++ components/Page/Home/index.tsx
@@ -1,74 +1,38 @@
 import {
-  Paper,
+  Button,
   styled,
-  TextField,
 } from "@mui/material";
 import { Overflow, Stretch } from "../../styled/containers";
-import Kuroshiro from "kuroshiro"
-import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji"
 import { FabToggleRomaji } from "./Buttons";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "../../../redux/store";
-import { contentActions } from "../../../redux/slices/contentSlice";
 import Row from "./Row";
+import { useState } from "react";
+import Editor from "./Editor";
 
 
 const ContentDiv = styled(Overflow(Stretch('div')))`
 `
 
-const kuroshiro = new Kuroshiro();
-(async () => await kuroshiro.init(new KuromojiAnalyzer({ dictPath: "dict/" })))()
-
 function Home() {
-  const dispatch = useDispatch()
-  const [source, setSource] = [
-    useSelector((s: RootState) => s.content.source),
-    (s: string[]) => dispatch(contentActions.setSource(s))
-  ]
-  const [furigana, setFurigana] = [
-    useSelector((s: RootState) => s.content.furigana),
-    (s: string[]) => dispatch(contentActions.setFurigana(s))
-  ]
-  const [romaji, setRomaji] = [
-    useSelector((s: RootState) => s.content.romaji),
-    (s: string[]) => dispatch(contentActions.setRomaji(s))
-  ]
+  const furigana = useSelector((s: RootState) => s.content.furigana)
+  const romaji = useSelector((s: RootState) => s.content.romaji)
   const romajiOn = useSelector((s: RootState) => s.layout.romajiOn)
-
-  const convert = (mode: string, to: string) =>
-    async (txt: string) => await kuroshiro.convert(txt, { mode, to })
-
+  const [editorOpen, setEditorOpen] = useState(false)
 
   return (
     <ContentDiv id='content-ctn'>
-      <Paper
-        elevation={1}
-        sx={{ p: 1 }}
+      <Button
+        onClick={() => setEditorOpen(true)}
       >
-        <TextField
-          fullWidth
-          multiline
-          label='Japanese'
-          value={source.join('\n')}
-          onChange={async e => {
-            const vals = e.target.value.split('\n')
-            setSource(vals)
-            try {
-              setFurigana(await Promise.all(vals.map(
-                async (txt: string) => await convert('furigana', 'hiragana')(txt))))
-              setRomaji(await Promise.all(vals.map(
-                async (txt: string) => await convert('furigana', 'romaji')(txt))))
-            } catch (err) {
-              console.log(err)
-            }
-          }}
-        />
-      </Paper>
+        Open Editor
+      </Button>
       {romajiOn ?
         <Row entry={romaji} />
         :
         <Row entry={furigana} />
       }
+      <Editor {...{ editorOpen, setEditorOpen }} />
       <FabToggleRomaji />
     </ContentDiv>
   )
