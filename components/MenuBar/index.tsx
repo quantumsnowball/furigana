3d8ca2fec0ac53a4f6b478c6a113661d2a529c7e
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-22 00:20:57 +0800

    feat: toggle between home and favorite button

--- components/MenuBar/index.tsx
+++ components/MenuBar/index.tsx
@@ -7,17 +7,21 @@ import {
 import MenuIcon from '@mui/icons-material/Menu'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeIcon from '@mui/icons-material/DarkMode'
+import StarIcon from '@mui/icons-material/Star'
+import EditIcon from '@mui/icons-material/Edit'
 import { useState } from "react"
 import MenuDrawer from "./MenuDrawer"
 import { useDispatch } from "react-redux"
 import { themeActions } from "../../redux/slices/themeSlice"
 import TitleSection from "./TitleSection"
+import { useRouter } from 'next/router'
 
 
 const MenuBar = () => {
   const dispatch = useDispatch()
   const theme = useTheme()
   const [menuOpen, setMenuOpen] = useState(false)
+  const router = useRouter()
 
   return (
     <>
@@ -34,6 +38,12 @@ const MenuBar = () => {
             <MenuIcon />
           </IconButton>
           <TitleSection />
+          <IconButton onClick={() => {
+            router.push(router.pathname === '/favorite' ? '/' : '/favorite')
+          }}>
+            {router.pathname === '/' ?
+              <StarIcon sx={{ color: '#fff' }} /> : <EditIcon sx={{ color: '#fff' }} />}
+          </IconButton>
           <IconButton onClick={() => dispatch(themeActions.toggleMode())}>
             {theme.palette.mode === 'light' ?
               <LightModeIcon sx={{ color: '#fff' }} /> : <DarkModeIcon />}
