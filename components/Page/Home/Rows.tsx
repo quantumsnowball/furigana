ca4b72bbc60191f45ab126e32662188f2963fef1
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-21 18:33:04 +0800

    refactor: move romaji toggle state into Rows

--- components/Page/Home/Rows.tsx
+++ components/Page/Home/Rows.tsx
@@ -6,10 +6,10 @@ import { v4 } from "uuid"
 import parse from "html-react-parser"
 import {
   FuriganaItem,
-  FuriganaItems,
   RomajiItem,
-  RomajiItems
 } from "../../../types/content"
+import { RootState } from "../../../redux/store"
+import { useSelector } from "react-redux"
 
 
 type RowProps = {
@@ -30,17 +30,21 @@ const Row = ({ item }: RowProps) =>
     </Typography>
   </Paper>
 
-type RowsProps = {
-  items: FuriganaItems | RomajiItems
-}
+const Rows = () => {
+  const furigana = useSelector((s: RootState) => s.content.furigana)
+  const romaji = useSelector((s: RootState) => s.content.romaji)
+  const romajiOn = useSelector((s: RootState) => s.layout.romajiOn)
+  const items = romajiOn ? romaji : furigana
 
-const Rows = ({ items }: RowsProps) =>
-  <Paper
-    elevation={1}
-    sx={{ m: 1, p: 1 }}
-  >
-    {items.map((r: FuriganaItem | RomajiItem) => <Row {...{ item: r }} />
-    )}
-  </Paper>
+  return (
+    <Paper
+      elevation={1}
+      sx={{ m: 1, p: 1 }}
+    >
+      {items.map((item: FuriganaItem | RomajiItem) => <Row {...{ item }} />
+      )}
+    </Paper>
+  )
+}
 
 export default Rows
--- components/Page/Home/index.tsx
+++ components/Page/Home/index.tsx
@@ -1,8 +1,6 @@
 import { styled, } from "@mui/material";
 import { Overflow, Stretch } from "../../styled/containers";
 import { FabToggleRomaji, ToggleEditor } from "./Buttons";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../redux/store";
 import Rows from "./Rows";
 import { useState } from "react";
 import Editor from "./Editor";
@@ -12,19 +10,12 @@ const ContentDiv = styled(Overflow(Stretch('div')))`
 `
 
 function Home() {
-  const furigana = useSelector((s: RootState) => s.content.furigana)
-  const romaji = useSelector((s: RootState) => s.content.romaji)
-  const romajiOn = useSelector((s: RootState) => s.layout.romajiOn)
   const [editorOpen, setEditorOpen] = useState(false)
 
   return (
     <ContentDiv id='content-ctn'>
       <ToggleEditor {...{ setEditorOpen }} />
-      {romajiOn ?
-        <Rows items={romaji} />
-        :
-        <Rows items={furigana} />
-      }
+      <Rows />
       <Editor {...{ editorOpen, setEditorOpen }} />
       <FabToggleRomaji />
     </ContentDiv>
