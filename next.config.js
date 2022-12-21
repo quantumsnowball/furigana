b638540287d196c4b8d25db810f3c2182d602b08
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-21 16:42:48 +0800

    feat: next-pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
+module.exports = withPWA({
+  // config
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+})
 
-module.exports = nextConfig
